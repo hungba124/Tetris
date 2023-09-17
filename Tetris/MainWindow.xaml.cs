db2eb7aac341c0f3852bd669ec2c238f1fb6ce12
Hungpv <phamvanhung124@gmail.com>
2023-09-17 19:55:26 +0700

    draw block

--- Tetris/MainWindow.xaml.cs
+++ Tetris/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Tetris
 {
@@ -81,14 +73,28 @@ namespace Tetris
 
         private void DrawGrid(GameGrid grid)
         {
-            for(int r = 0;r < grid.Rows; r++)
+            for (int r = 0; r < grid.Rows; r++)
             {
                 for (int c = 0; c < grid.Columns; c++)
                 {
                     int id = grid[r, c];
-                    imageControls[r, c] = tileImages[id];
+                    imageControls[r, c].Source = tileImages[id];
                 }
             }
+        }
+
+        private void DrawBlock(Block block)
+        {
+            foreach (Position p in block.TilePositions())
+            {
+                imageControls[p.Row, p.Column].Source = tileImages[block.Id];
+            }
+        }
+
+        private void Draw(GameState gameState)
+        {
+            DrawGrid(gameState.GameGrid);
+            DrawBlock(gameState.CurrentBlock);
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
@@ -98,7 +104,7 @@ namespace Tetris
 
         private void GameCanvas_Loaded(object sender, RoutedEventArgs e)
         {
-
+            Draw(gameState);
         }
 
         private void PlayAgain_Click(object sender, RoutedEventArgs e)
